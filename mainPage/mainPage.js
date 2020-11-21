8b3cc1ed7cd0643d795e0fbd972442042ac0c06f
pavleny <72353664+pavleny@users.noreply.github.com>
2020-11-21 23:45:31 +0200

    Add JS for theme in Main

--- mainPage/mainPage.js
+++ mainPage/mainPage.js
@@ -14,3 +14,18 @@ let arrSession = [];
 let requestURL2 = 'http://26.116.247.102:8080/section';
 let request2 = new XMLHttpRequest();
 request2.open('GET', requestURL2, true);
+
+
+
+
+function goToTheme() {
+    document.location.href = '../themePage/themePage.html';
+    sessionStorage.setItem('theme', this.id);
+}
+
+window.onload = function() {
+    alert(localStorage.getItem('nick'));
+    alert(localStorage.getItem('fullName'));
+    alert(localStorage.getItem('email'));
+    alert(localStorage.getItem('password'));
+}
