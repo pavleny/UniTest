b82379ca79d2e816efcb4acb4138acef5a29ff04
pavleny <72353664+pavleny@users.noreply.github.com>
2020-11-25 12:54:02 +0200

    Add sections and courses

--- loginPage/loginPage.js
+++ loginPage/loginPage.js
@@ -2,3 +2,218 @@ function toggleForm() {
     let container = document.querySelector('.container');
     container.classList.toggle('active');
 }
+
+//-----------------------Регистрация--------------------------------
+//Warnings
+let nameError1 = document.getElementById('nameError1');
+let nameError2 = document.getElementById('nameError2');
+let fullError1 = document.getElementById('fullError1');
+let emailError1 = document.getElementById('emailError1');
+let emailError2 = document.getElementById('emailError2');
+let emailError3 = document.getElementById('emailError3');
+let passError1 = document.getElementById('passError1');
+let passError2 = document.getElementById('passError2');
+
+let requestURL;
+
+let userName = document.getElementById('userName');
+let fullName = document.getElementById('fullName');
+let email = document.getElementById('email');
+let pass1 = document.getElementById('pass1');
+let pass2 = document.getElementById('pass2');
+let submit = document.getElementById('submit');
+//let image = document.getElementById('image');
+
+let emailReg = /\S+@\S+\.\S+/;
+let passReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,}/;
+
+let errors = 0; 
+
+let f1, f2, f3, f4, f5, f6;
+let users = {};
+
+submit.addEventListener('click', function() {
+    f1 = checkPass(pass1, pass2);
+    f7 = checkEmpty(email);
+    f2 = checkEmail(email);
+    f6 = checkSimilarEmail(email);
+    f3 = checkNames(userName);
+    f4 = checkSimilarName(userName);
+    f5 = checkFullName(fullName);
+});
+submit.addEventListener('click', sendForm);
+
+function checkPass(pass_1, pass_2) {
+    if(!passReg.test(pass_1.value)) {
+        pass_1.style = 'border: 2px solid red';
+        pass_2.style = 'border: 2px solid red';
+        passError1.style = 'display: none';
+        passError2.style = 'display: block';
+        return false;
+    } else if(pass_1.value === pass_2.value) {
+        pass_1.style = 'border: 2px solid green';
+        pass_2.style = 'border: 2px solid green';
+        passError1.style = 'display: none';
+        passError2.style = 'display: none';
+        return true;
+    } 
+    else {
+        pass_1.style = 'border: 2px solid red';
+        pass_2.style = 'border: 2px solid red';
+        passError2.style = 'display: none';
+        passError1.style = 'display: block';
+        return false;
+    }
+}
+
+function checkEmail(email) {
+    if(emailReg.test(email.value)) {
+        email.style = 'border: 2px solid green';
+        emailError3.style = 'display: none';
+        return true;
+    } else {
+        email.style = 'border: 2px solid red';
+        emailError3.style = 'display: block';
+        return false;
+    }
+}
+
+function checkNames(userName) {
+    if(userName.value === '') {
+        userName.style = 'border: 2px solid red';
+        nameError2.style = 'display: block';
+        return false;
+    }else {
+        nameError2.style = 'display: none';
+        userName.style = 'border: 2px solid green';
+        return true;
+    }
+}
+
+function checkEmpty(email) {
+    if(email.value === '') {
+        email.style = 'border: 2px solid red';
+        emailError2.style = 'display: block';
+        return false;
+    }else {
+        emailError2.style = 'display: none';
+        email.style = 'border: 2px solid green';
+        return true;
+    }
+}
+
+function checkFullName(fullName) {
+    if(fullName.value === '') {
+        fullName.style = 'border: 2px solid red';
+        fullError1.style = 'display: block'; 
+        return false;
+    } else {
+        fullError1.style = 'display: none';
+        fullName.style = 'border: 2px solid green';
+        return true;
+    }
+}
+
+function checkSimilarName(userName) {
+    let count = 0;
+    for(let i = 0; i < users.length; i++) {
+        if(users[i]['nickname'] === userName.value) {
+            nameError1.style = 'display: block';
+            userName.style = 'border: 2px solid red';
+            return false;
+        } 
+    }
+    if(userName.value === '') {
+        nameError1.style = 'display: none';
+        userName.style = 'border: 2px solid red';
+        return false;
+    }else {
+        userName.style = 'border: 2px solid green';
+        return true;
+    }
+}
+
+function checkSimilarEmail(email) {
+    for(let i = 0; i < users.length; i++) {
+        if(users[i]['email'] === email.value) {
+            emailError1.style = 'display: block';
+            email.style = 'border: 2px solid red';
+            return false;
+        }
+    }
+    if(email.value === '') {
+        emailError1.style = 'display: none';
+        email.style = 'border: 2px solid red';
+        return false;
+    }else {
+        emailError1.style = 'display: none';
+        return true;
+    }
+}
+
+function sendForm() {
+    if(f1 && f2 && f3 && f4 && f5 && f6 && f7) {
+        sendRequest();
+        document.location.href = '../mainPage/mainPage.html';
+    } else {
+        alert('Error!');
+    }
+}
+
+window.onload = function() {
+    requestURL = 'http://26.116.247.102:8080/account';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL, true);
+
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        var section = request.response;
+        populateUsers(section);
+    }
+
+    function populateUsers(jsonObj) {
+        users = JSON.parse(JSON.stringify(jsonObj));
+    }
+}
+
+function sendRequest() {
+    localStorage.clear();
+    localStorage.setItem('nick', userName.value);
+    localStorage.setItem('fullName', fullName.value);
+    localStorage.setItem('email', email.value);
+    localStorage.setItem('password', pass1.value);
+   // localStorage.setItem('image', image.value);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", `${requestURL}?`, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(`nickname=${userName.value}&email=${email.value}&password=${pass1.value}&role=User&bonuses=0&fullName=${fullName.value}&activationCode`);
+}
+//--------------------------------------------------------------------------
+
+
+
+//-------------------Авторизация---------------------------------------------
+
+let userName_in = document.getElementById('userName-in');
+let pass_in = document.getElementById('pass-in');
+let login = document.getElementById('log-in');
+let text_in = document.getElementById('login-error');
+
+function userLogIn() {
+    for(let i = 0; i< users.length; i++) {
+        if(userName_in.value === users[i]['nickname'] && pass_in.value === users[i]['password']) {
+            localStorage.clear();
+            localStorage.setItem('nick', userName_in.value);
+            localStorage.setItem('fullName', users[i]['fullname']);
+            localStorage.setItem('email', users[i]['email']);
+            localStorage.setItem('password', pass_in.value);
+
+            document.location.href = '../mainPage/mainPage.html';
+        } else {
+            userName_in.style = 'border: 2px solid red';
+            pass_in.style = 'border: 2px solid red';
+            text_in.style = 'display: block';
+        }
+    }
+}
--- mainPage/mainPage.js
+++ mainPage/mainPage.js
@@ -6,7 +6,7 @@ menu.onclick = function burger() {
         x.className = 'topnav';
     }
 }
-
+/*
 let idSection;
 let isOpened = false;
 let arrSession = [];
@@ -15,12 +15,176 @@ let requestURL2 = 'http://26.116.247.102:8080/section';
 let request2 = new XMLHttpRequest();
 request2.open('GET', requestURL2, true);
 
+request2.responseType = 'json';
+request2.send();
+request2.onload = function() {
+    var section2 = request2.response;
+    populateSections(section2);
+    //checkList();
+  }
 
+  function populateSections(jsonObj) {
+      for(let i = 0; i < jsonObj.length; i++) {
+        let ul = document.createElement('ul');
+        ul.innerHTML = `${jsonObj[i]['name']}`;
+        ul.classList.add('options');
+        ul.id = `${i}s`;
+        document.querySelector('.sections-list').appendChild(ul);
 
+        let hr = document.createElement('hr');
+        document.getElementById(`${i}s`).appendChild(hr);
+
+        for(let j = 0; j < jsonObj[i]['themes'].length; j++) {
+            let li = document.createElement('li');
+            li.innerHTML = `${jsonObj[i]['themes'][j]['name']}`;
+            li.classList.add('links');
+            li.addEventListener('click', goToTheme);
+            li.id = jsonObj[i]['themes'][j]['id_theme'];
+            document.getElementById(`${i}s`).appendChild(li);
+        }
+      }
+      let sections = document.getElementsByClassName('options');
+      for(let i = 0; i < sections.length; i++) {
+          if(sections[i].children.length < 2) {
+              let h3 = document.createElement('h3');
+              h3.innerHTML = 'Urokov Poke NEt';
+              document.getElementById(`${i}s`).appendChild(h3);       
+            } 
+        } 
+    }
 
 function goToTheme() {
     document.location.href = '../themePage/themePage.html';
     sessionStorage.setItem('theme', this.id);
+}
+*/
+
+let cources = [];
+let recommended = [];
+
+requestURL = 'http://26.116.247.102:8080/section';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL, true);
+
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    var section = request.response;
+    populateSections(section);
+}
+
+function populateSections(jsonObj) {
+    cources = JSON.parse(JSON.stringify(jsonObj));
+    for(let i = 0; i < cources.length; i++) {
+        let mainDiv = document.createElement('div');
+        mainDiv.classList.add(`sub-container${i}`);
+        mainDiv.classList.add(`sub`);
+        document.querySelector('.container').appendChild(mainDiv);
+
+        let leftDiv = document.createElement('div');
+        leftDiv.classList.add(`sub-container-left${i}`);
+        leftDiv.classList.add(`sub-left`);
+        leftDiv.style = 'width: 50%';
+        document.querySelector(`.sub-container${i}`).appendChild(leftDiv);
+
+        let rightDiv = document.createElement('div');
+        rightDiv.classList.add(`sub-container-right${i}`);
+        rightDiv.classList.add(`sub-right`);
+        document.querySelector(`.sub-container${i}`).appendChild(rightDiv);
+
+        if(i%2 !== 0) {
+            rightDiv.style = 'order: -1';
+        }
+
+        let h1 = document.createElement('h1');
+        h1.classList.add('sub-heading');
+        h1.innerHTML = `${cources[i]['name']}`;
+        document.querySelector(`.sub-container-left${i}`).appendChild(h1);
+
+        let ul = document.createElement('ul');
+        ul.classList.add(`sub-list${i}`);
+        ul.classList.add(`list`);
+        ul.id = `${i}s`;
+        document.querySelector(`.sub-container-left${i}`).appendChild(ul);
+
+        for(let j = 0; j < cources[i]['themes'].length; j++) {
+
+            let li = document.createElement('li');
+            li.classList.add(`li-${j}-${i}`);
+            document.querySelector(`.sub-list${i}`).appendChild(li);
+
+            let a = document.createElement('a');
+            a.innerHTML = `${cources[i]['themes'][j]['name']}`;
+            a.href = '../themePage/themePage.html';
+            a.classList.add('ref');
+            document.querySelector(`.li-${j}-${i}`).appendChild(a);
+        }
+
+        let img = document.createElement('img');
+        img.src = `../img/mainImg/${Math.floor(Math.random() * 5)}.jpg`;
+        img.style = 'width: 100%; height: 100%;';
+        img.classList.add('images');
+        document.querySelector(`.sub-container-right${i}`).appendChild(img);
+    }
+    let sections = document.getElementsByClassName('list');
+      for(let i = 0; i < sections.length; i++) {
+          if(sections[i].children.length < 1) {
+              let h3 = document.createElement('h3');
+              h3.innerHTML = 'Urokov Poke NEt';
+              document.getElementById(`${i}s`).appendChild(h3);       
+            } 
+        } 
+}
+
+requestURL2 = 'http://26.116.247.102:8080/theme/top';
+let request2 = new XMLHttpRequest();
+request2.open('GET', requestURL2, true);
+
+request2.responseType = 'json';
+request2.send();
+request2.onload = function() {
+    var section = request2.response;
+    populateRecommended(section);
+}
+
+function populateRecommended(jsonObj) {
+    recommended = JSON.parse(JSON.stringify(jsonObj));
+
+    for(let i = 0; i < recommended.length; i++) {
+        let sub_popular = document.createElement('div');
+        sub_popular.classList.add(`sub_popular${i}`);
+        sub_popular.classList.add(`sub_popular_each`);
+        document.querySelector(`.popular`).appendChild(sub_popular);
+
+        let imgDiv = document.createElement('div');
+        imgDiv.classList.add(`img-div-${i}`);
+        imgDiv.classList.add('img-div-each');
+        document.querySelector(`.sub_popular${i}`).appendChild(imgDiv);
+
+        let image = document.createElement('img');
+        image.src = `http://26.116.247.102:8080/image/${recommended[i]['image']}`;
+        image.classList.add('popular-img');
+        document.querySelector(`.img-div-${i}`).appendChild(image);
+
+        let h2 = document.createElement('h2');
+        h2.classList.add('popular-heading');
+        h2.innerHTML = `${recommended[i]['name']}`;
+        document.querySelector(`.sub_popular${i}`).appendChild(h2);
+
+        let p = document.createElement('p');
+        p.classList.add('popular-description');
+        p.innerHTML = `${recommended[i]['description']}`;
+        document.querySelector(`.sub_popular${i}`).appendChild(p);
+
+        let buttonDiv = document.createElement('div');
+        buttonDiv.classList.add(`button-div-${i}`);
+        buttonDiv.classList.add('button-div-each');
+        document.querySelector(`.sub_popular${i}`).appendChild(buttonDiv);
+        let button = document.createElement('button');
+        button.innerHTML = 'Перейти';
+        button.classList.add('buttons-popular');
+        document.querySelector(`.button-div-${i}`).appendChild(button);
+    }
 }
 
 window.onload = function() {
