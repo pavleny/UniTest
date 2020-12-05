027aa84d33c14175ad24c11c7b5f3cb0f5232fe3
AlexPozitivFE <63417970+AlexPozitivFE@users.noreply.github.com>
2020-12-05 22:15:19 +0200

    Add JS to download questions and answers

--- taskPage/taskPage.js
+++ taskPage/taskPage.js
@@ -13,3 +13,51 @@ request.onload = function() {
     //checkList();
 }
 
+<<<<<<< Updated upstream
+=======
+function populateTasks(jsonObj) {
+    for(let i = 0; i < jsonObj.length; i++) {
+        if(jsonObj[i]['id_level'] == levelId) {
+            let div = document.createElement('div');
+            div.classList.add(`questions${i}`);
+            div.classList.add('row');
+            div.id = jsonObj[i]['id_question']
+            document.querySelector('.question-container').appendChild(div);
+
+            let img = document.createElement('img');
+            img.classList.add('question-image');
+            if(jsonObj[i]['image'] == '') {
+                img.src = '../img/fon.png';
+            } else {
+                img.src = `http://26.116.247.102:8080/image/${jsonObj[i]['image']}`;
+            }
+            document.querySelector(`.questions${i}`).appendChild(img);
+
+            let p = document.createElement('p');
+            p.innerHTML = `${jsonObj[i]['text']}`;
+            p.classList.add(`${i}`);
+            document.querySelector(`.questions${i}`).appendChild(p);
+
+            for(let j = 0; j < jsonObj[i]['answers'].length; j++) {
+                
+                let div2 = document.createElement('div');
+                div2.classList.add(`variants-${j}-${i}`);
+                document.querySelector(`.questions${i}`).appendChild(div2);
+
+                let input = document.createElement('input');
+                input.type = 'checkbox';
+                input.id = `${jsonObj[i]['answers'][j]['isCorrect']}`;
+                input.classList.add(`s${j}`);
+                input.classList.add(`inputs`);
+                document.querySelector(`.variants-${j}-${i}`).appendChild(input);
+
+                let span = document.createElement('span');
+                span.innerHTML = `${jsonObj[i]['answers'][j]['text']}`;
+                document.querySelector(`.variants-${j}-${i}`).appendChild(span);
+            }
+
+           
+        }
+    }
+}
+>>>>>>> Stashed changes
